f4ac1508d69b7128b89b7c5fefd3ba1c4fe5e291
LYM <letusgo126@126.com>
2022-08-16 20:51:09 +0800

    1.3

--- configs/_base_/default_runtime.py
+++ configs/_base_/default_runtime.py
@@ -12,7 +12,7 @@ custom_hooks = [dict(type='NumClassCheckHook')]
 dist_params = dict(backend='nccl')
 log_level = 'INFO'
 load_from = None
-resume_from =  None
+resume_from = None
 workflow = [('train', 1)]
 
 # disable opencv multithreading to avoid system being overloaded
