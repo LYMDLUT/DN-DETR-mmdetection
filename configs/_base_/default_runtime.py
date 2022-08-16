dad3ecc442b283eb9fc48e0712e8c553dc214c02
LYM <letusgo126@126.com>
2022-08-16 20:48:52 +0800

    1.3

--- configs/_base_/default_runtime.py
+++ configs/_base_/default_runtime.py
@@ -12,7 +12,7 @@ custom_hooks = [dict(type='NumClassCheckHook')]
 dist_params = dict(backend='nccl')
 log_level = 'INFO'
 load_from = None
-resume_from =  None #'/media/data4/lym/mmdetection-detr/work_dirs/Conditional_detr_r50_8x2_50e_coco/epoch_10.pth'
+resume_from =  None
 workflow = [('train', 1)]
 
 # disable opencv multithreading to avoid system being overloaded
